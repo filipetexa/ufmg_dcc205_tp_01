ae0f513beb5943a5f35bd03f6a66e2326a02d098
filipetexa <filipeteixeirasouza@gmail.com>
2024-12-04 15:00:15 -0300

    [update] format document memlog.c

--- src/memlog.c
+++ src/memlog.c
@@ -11,40 +11,42 @@ void clkDifMemLog(struct timespec t1, struct timespec t2,
 // Entrada: t1, t2
 // Saida: res
 {
-    if (t2.tv_nsec < t1.tv_nsec)
-    {
-        // ajuste necessario, utilizando um segundo de tv_sec
-        res->tv_nsec = 1000000000 + t2.tv_nsec - t1.tv_nsec;
-        res->tv_sec = t2.tv_sec - t1.tv_sec - 1;
-    }
-    else
-    {
-        // nao e necessario ajuste
-        res->tv_nsec = t2.tv_nsec - t1.tv_nsec;
-        res->tv_sec = t2.tv_sec - t1.tv_sec;
-    }
+  if (t2.tv_nsec < t1.tv_nsec)
+  {
+    // ajuste necessario, utilizando um segundo de tv_sec
+    res->tv_nsec = 1000000000 + t2.tv_nsec - t1.tv_nsec;
+    res->tv_sec = t2.tv_sec - t1.tv_sec - 1;
+  }
+  else
+  {
+    // nao e necessario ajuste
+    res->tv_nsec = t2.tv_nsec - t1.tv_nsec;
+    res->tv_sec = t2.tv_sec - t1.tv_sec;
+  }
 }
 
-
-int iniciaMemLog(char * nome)
+int iniciaMemLog(char *nome)
 // Descricao: inicializa o registro de acessos, abrindo o arquivo nome
 // Entrada: nome
 // Saida: nao tem
 { // escolhe modo do relogio
   ml.clk_id = CLOCK_MONOTONIC;
   // abre arquivo de registro e verifica se foi aberto corretamente
-  ml.log = fopen(nome,"wt");
-  erroAssert(ml.log != NULL,"Cannot open memlog output");
+  ml.log = fopen(nome, "wt");
+  erroAssert(ml.log != NULL, "Cannot open memlog output");
   // captura o tempo inicial do registro
   struct timespec tp;
-  int result = clock_gettime(ml.clk_id,&tp);
-  ml.inittime.tv_sec = tp.tv_sec;  ml.inittime.tv_nsec = tp.tv_nsec;
+  int result = clock_gettime(ml.clk_id, &tp);
+  ml.inittime.tv_sec = tp.tv_sec;
+  ml.inittime.tv_nsec = tp.tv_nsec;
   // inicializa variaveis do TAD
-  ml.count = 1;   ml.ativo = MLATIVO;  ml.fase = 0;
+  ml.count = 1;
+  ml.ativo = MLATIVO;
+  ml.fase = 0;
   // imprime registro inicial
-  int retprint = fprintf(ml.log,"I %ld %ld.%.9ld\n",
-                      ml.count,tp.tv_sec,tp.tv_nsec);
-  erroAssert(retprint>=0,"Nao foi possivel escrever registro");
+  int retprint = fprintf(ml.log, "I %ld %ld.%.9ld\n",
+                         ml.count, tp.tv_sec, tp.tv_nsec);
+  erroAssert(retprint >= 0, "Nao foi possivel escrever registro");
   return result;
 }
 
@@ -52,21 +54,26 @@ int ativaMemLog()
 // Descricao: ativa o registro de acessos
 // Entrada: nao tem
 // Saida: MLATIVO
-{ ml.ativo = MLATIVO;
+{
+  ml.ativo = MLATIVO;
   return MLATIVO;
 }
+
 int desativaMemLog()
 // Descricao: desativa o registro de acessos
 // Entrada: nao tem
 // Saida: MLINATIVO
-{ ml.ativo = MLINATIVO;
+{
+  ml.ativo = MLINATIVO;
   return MLINATIVO;
 }
+
 int defineFaseMemLog(int f)
 // Descricao: define a fase de registro de acessos
 // Entrada: f
 // Saida: valor de f
-{ ml.fase = f;
+{
+  ml.fase = f;
   return f;
 }
 
@@ -75,18 +82,19 @@ int leMemLog(long int pos, long int tam, int id)
 // Entrada: pos,tam
 // Saida: resultado da obtencao do relogio
 { // verifica se registro esta ativo
-  if (ml.ativo == MLINATIVO) return 0;
+  if (ml.ativo == MLINATIVO)
+    return 0;
   // captura tempo atual
   struct timespec tp, tdif;
-  int result = clock_gettime(ml.clk_id,&tp);
+  int result = clock_gettime(ml.clk_id, &tp);
   // calcula a diferencao com tempo inicial, para economia de armazenamento
-  clkDifMemLog(ml.inittime,tp,&tdif);
+  clkDifMemLog(ml.inittime, tp, &tdif);
   // atualiza contador
   ml.count++;
   // imprime registro
-  int retprint = fprintf(ml.log,"L %d %ld %d %ld.%.9ld %ld %ld\n",
-       ml.fase, ml.count, id, tdif.tv_sec, tdif.tv_nsec, pos, tam);
-  erroAssert(retprint>=0,"Nao foi possivel escrever registro");
+  int retprint = fprintf(ml.log, "L %d %ld %d %ld.%.9ld %ld %ld\n",
+                         ml.fase, ml.count, id, tdif.tv_sec, tdif.tv_nsec, pos, tam);
+  erroAssert(retprint >= 0, "Nao foi possivel escrever registro");
   return result;
 }
 
@@ -95,18 +103,19 @@ int escreveMemLog(long int pos, long int tam, int id)
 // Entrada: pos, tam
 // Saida: resultado da obtencao do relogio
 { // verifica se registro esta ativo
-  if (ml.ativo == MLINATIVO) return 0;
+  if (ml.ativo == MLINATIVO)
+    return 0;
   // captura tempo atual
-  struct timespec tp,tdif;
-  int result = clock_gettime(ml.clk_id,&tp);
+  struct timespec tp, tdif;
+  int result = clock_gettime(ml.clk_id, &tp);
   // calcula a diferenca com tempo inicial, para economia de armazenamento
-  clkDifMemLog(ml.inittime,tp,&tdif);
+  clkDifMemLog(ml.inittime, tp, &tdif);
   // atualiza contador
   ml.count++;
   // imprime registro
-  int retprint = fprintf(ml.log,"E %d %ld %d %ld.%.9ld %ld %ld\n",
-       ml.fase, ml.count, id, tdif.tv_sec, tdif.tv_nsec, pos, tam);
-  erroAssert(retprint>=0,"Nao foi possivel escrever registro");
+  int retprint = fprintf(ml.log, "E %d %ld %d %ld.%.9ld %ld %ld\n",
+                         ml.fase, ml.count, id, tdif.tv_sec, tdif.tv_nsec, pos, tam);
+  erroAssert(retprint >= 0, "Nao foi possivel escrever registro");
   return result;
 }
 
@@ -116,18 +125,18 @@ int finalizaMemLog()
 // Saida: resultado da obtencao do relogio
 { // captura o tempo atual
   struct timespec tp, tdif;
-  int result = clock_gettime(ml.clk_id,&tp);
+  int result = clock_gettime(ml.clk_id, &tp);
   // calcula a diferenca com tempo inicial, para economia de armazenamento
-  clkDifMemLog(ml.inittime,tp,&tdif);
+  clkDifMemLog(ml.inittime, tp, &tdif);
   // atualiza contador
   ml.count++;
   // imprime registros finais
-  int retprint = fprintf(ml.log,"F %ld %ld.%.9ld %ld.%.9ld\n", ml.count, 
-                 tp.tv_sec,tp.tv_nsec, tdif.tv_sec,tdif.tv_nsec);
-  erroAssert(retprint>=0,"Nao foi possivel escrever registro");
+  int retprint = fprintf(ml.log, "F %ld %ld.%.9ld %ld.%.9ld\n", ml.count,
+                         tp.tv_sec, tp.tv_nsec, tdif.tv_sec, tdif.tv_nsec);
+  erroAssert(retprint >= 0, "Nao foi possivel escrever registro");
   // fecha arquivo de registro
   int retclose = fclose(ml.log);
-  erroAssert(retclose == 0,"Nao foi possivel fechar o arquivo de registro");
+  erroAssert(retclose == 0, "Nao foi possivel fechar o arquivo de registro");
   // atualiza variavel de estado
   ml.ativo = MLINATIVO;
   return result;
